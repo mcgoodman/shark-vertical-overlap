4f15d2e3becb91023dfda129f5a7f8751b7b6e8c
Maurice Goodman <goodman.maurice@gmail.com>
2021-07-02 17:14:47 -0700

    more efficient calculation of distance matrices

--- src/shark_vertical_overlap.R
+++ src/shark_vertical_overlap.R
@@ -11,6 +11,7 @@
 library(tidyverse)
 library(cluster)
 library(factoextra)
+library(proxy)
 
 ## Read in metadata
 metadata <- read.csv("./data/Individual_tag_metadata.csv")
@@ -40,66 +41,10 @@ med_depths <- na.omit(med_depths) ## 3 NA date values
 
 write.csv(med_depths, "./output/median_depths.csv")
 
-## Define overlap functions ---------------------------------------------------
-
-#' Bhattacharya's coefficient
-#' Measures similarity between two probability distributions
-#' @param x A vector of depths for the first species
-#' @param y A vector of depths for the second species
-#' @param bin Logical; use if data are not already binned by depth
-#' @param breaks The breaks used to bin depths for both species, if necessary
-#' @citation Bhattacharyya, A. (1943). Bulletin of the Calcutta Mathematical Society, 35, 99–110
-bhattacharya_depth <- function(x, y, bin = FALSE, breaks) {
-  if (bin) {
-    x <- cut(x, breaks)
-    y <- cut(y, breaks)
-  } 
-  xdens <- as.numeric(table(x)/length(x))
-  ydens <- as.numeric(table(y)/length(y))
-  sum(sqrt(xdens * ydens))
-}
-
-#' Schoener's D
-#' Measures how equally two species use space relative to availability
-#' @param x A vector of depths for the first species
-#' @param y A vector of depths for the second species
-#' @param bin Logical; use if data are not already binned by depth
-#' @param breaks The breaks used to bin depths for both species, if necessary
-#' @citation Schoener, T. W. (1970). Ecology, 51, 408–418
-schoener_depth <- function(x, y, bin = FALSE, breaks) {
-  if (bin) {
-    x <- cut(x, breaks)
-    y <- cut(y, breaks)
-  } 
-  xdens <- as.numeric(table(x)/length(x))
-  ydens <- as.numeric(table(y)/length(y))
-  1 - 0.5 * sum(abs(xdens - ydens))
-}
-
-#' Euclidian Distance
-#' Distance between proportion depth utilization in n-bin space
-#' Provided for comparability with Madigan et al. (2020)
-#' @param x A vector of depths for the first species
-#' @param y A vector of depths for the second species
-#' @param bin Logical; use if data are not already binned by depth
-#' @param breaks The breaks used to bin depths for both species, if necessary
-#' @citation Madigan et al. (2020). ICES Journal of Marine Science
-euclidian_depth <- function(x, y, bin = FALSE, breaks) {
-  if (bin) {
-    x <- cut(x, breaks)
-    y <- cut(y, breaks)
-  } 
-  xdens <- as.numeric(table(x)/length(x))
-  ydens <- as.numeric(table(y)/length(y))
-  sqrt(sum((xdens - ydens)^2))
-}
-
-## Calculate overlap between species ------------------------------------------
-
-## quick function to round number up by 10
-ceiling10 <- function(x) if (x %% 10 == 0) return(x) else return((10 - x %% 10) + x)
+## Format and summarize data ------------------------------------------
 
 ## define depth breaks
+ceiling10 <- function(x) if (x %% 10 == 0) return(x) else return((10 - x %% 10) + x)
 depth_max <- ceiling10(max(med_depths$Depth))
 depth_breaks <- seq(0, depth_max, by = 10)
 
@@ -112,30 +57,35 @@ species_counts <- med_depths %>% group_by(Species) %>% summarize(n = length(uniq
 species_sub <- species_counts$Species[species_counts$n >= 5]
 n_sp <- length(species_sub)
 
-## Combinations of species for which to calculate overlap metrics
-species_combn <- expand.grid(species1 = species_sub, species2 = species_sub)
-species_combn$bhattacharya <- species_combn$schoener <- species_combn$euclidian <- NA
+## Summarize depth distribution for each species, with depth bins as columns
+depth_binned <- med_depths %>% 
+  filter(Species %in% species_sub) %>% 
+  group_by(Species) %>% 
+  mutate(n_obs = n()) %>% 
+  group_by(Species, Depth_bin, n_obs) %>% 
+  tally() %>% 
+  mutate(p = n/n_obs) %>% select(-n_obs, -n) %>%
+  pivot_wider(names_from = Depth_bin, values_from = p, values_fill = 0) %>% 
+  column_to_rownames("Species")
 
-## Compute overlap metrics for each pair of species
-for (i in 1:nrow(species_combn)) {
-  x <- med_depths$Depth_bin[med_depths$Species == species_combn$species1[i]]
-  y <- med_depths$Depth_bin[med_depths$Species == species_combn$species2[i]]
-  species_combn$bhattacharya[i] <- bhattacharya_depth(x, y)
-  species_combn$schoener[i] <- schoener_depth(x, y)
-  species_combn$euclidian[i] <- euclidian_depth(x, y)
-}
+## Dissimilarity matrices & Hierarchical clustering ---------------------------
 
-write.csv(species_combn, "./output/overlap_metrics.csv")
+## Bhattacharya's coefficient - dissimilarity function
+## Measures similarity between two probability distributions
+bhattacharya <- function(x, y) 1 - sum(sqrt(x * y))
 
-## Hierarchical clustering ----------------------------------------------------
+## Schoener's D - dissimilarity function
+## Measures how equally two species use space relative to availability
+schoener <- function(x, y) 1 - (1 - 0.5 * sum(abs(x - y)))
 
 ## Create distance matrices for each metric
-## Subtract Bhattacharya and Schoener's D from 1 to change from similarity metric to dissimilarity
 dists <- list(
-  bhattacharya = as.dist(1 - matrix(species_combn[,"bhattacharya"], n_sp, n_sp)),
-  schoener = as.dist(1 - matrix(species_combn[,"schoener"], n_sp, n_sp)),
-  euclidian = as.dist(matrix(species_combn[,"euclidian"], n_sp, n_sp))
+  bhattacharya = dist(depth_binned, method = bhattacharya),
+  schoener = dist(depth_binned, method = schoener), 
+  euclidian = dist(depth_binned)
 )
+
+saveRDS(dists, "./output/distance_matrices.rds")
 
 ## Hierarchical clustering for each metric
 clusters <- lapply(dists, hclust, method = "complete")
